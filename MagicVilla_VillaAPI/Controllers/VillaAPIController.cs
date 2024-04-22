3b263f7d3e039a57d70c500cc02bbb3ad6ab8e3f
Omkar <omkarberi48@gmail.com>
2024-04-22 12:47:40 +0530

    First End Point

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,26 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[Controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        }
+    }
+}
